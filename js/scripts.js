c9b9c88be1dcdc21ee6ab380f575bd1805f48e1b
santa <santa@northpole.com>
2019-03-01 17:52:43 +0300

    style the divs

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// onclick play the roll $ pass buttons appear
+// first chance goes to player one
+// clicks roll n a random number appears.player can roll or pass.if its 1 can't continue to play
+// if player passes the count is added to their total Score
+// if player gets a 1 no count is added to their total score
+// first player to reach 100 points wins
